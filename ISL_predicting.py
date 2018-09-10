72ac522a0e245b8ee45f6736976ea451cd592b27
Simurgh818 <sina.dabiri@berkeley.edu>
2018-09-10 14:04:25 -0700

    Training files updated to have virtual environment in XML file.

---
+++ ISL_predicting.py
@@ -0,0 +1,99 @@
+import subprocess
+import argparse
+import sys, os
+from datetime import datetime
+from time import strftime
+
+dataset_eval_dir = ' '
+output_dir = ' '
+
+
+def main():
+
+	# ~/venvs/tensorflow/SinaFlow/bin/activate
+	virtual_env_path_active = 'source ~/venvs/tensorflow/SinaFlow/bin/activate;  '
+	
+	# /home/sinadabiri/venvs/in-silico-labeling-master
+	base_directory_path = 'cd /home/sinadabiri/venvs/in-silico-labeling-master; '
+
+	cmd1 = [base_directory_path + 'bazel shutdown;']
+	process1 = subprocess.Popen(cmd1, shell=True, stdout=subprocess.PIPE)
+	process1.wait()
+
+	base_dir = 'export BASE_DIRECTORY=/mnt/finkbeinernas/robodata/Sina/in-silico-labeling/isl;  '
+	
+
+	baz_cmd = [base_directory_path + virtual_env_path_active + base_dir + 'bazel run isl:launch -- \
+	  --alsologtostderr \
+	  --base_directory $BASE_DIRECTORY \
+	  --mode EVAL_EVAL \
+	  --metric INFER_FULL \
+	  --stitch_crop_size '+ image_crop_size + ' ' +  model_type +' \
+	  --output_path '+ output_dir + ' \
+	  --read_pngs \
+	  --dataset_eval_directory ' + dataset_eval_dir + '  \
+	  --infer_channel_whitelist ' + infer_chan + ' \
+    > ' + output_dir + '/predicting_output_'+ mod + '_'+ date_time +'_'+ image_crop_size +'_condition_b_sample_images.txt \
+    2> ' + output_dir + '/predicting_error_'+ mod + '_'+ date_time +'_'+ image_crop_size +'_condition_b_sample_images.txt;']
+	
+	process = subprocess.Popen(baz_cmd, shell=True, stdout=subprocess.PIPE)
+	process.wait()
+	output = process.communicate()[0]
+
+	cmd3 = [base_directory_path + 'bazel shutdown;']
+	process3 = subprocess.Popen(cmd3, shell=True, stdout=subprocess.PIPE)
+	process3.wait()
+
+	return 
+
+
+if __name__ == '__main__':
+  
+  # ----Parser-----------------------
+	parser = argparse.ArgumentParser(description="ISL Predicting.")
+	parser.add_argument("crop_size", help="Image Crop Size.")
+	parser.add_argument("model", help="Model type.")
+	parser.add_argument("output_path", help="Output Image Folder location.")
+	parser.add_argument("dataset_eval_path", help="Folder path to images directory.")
+	parser.add_argument("infer_channels", help="Channel Inferences.")
+
+	args = parser.parse_args()
+
+  # ----Initialize parameters------------------
+
+	image_crop_size = args.crop_size
+	model_type = args.model
+	mod = args.model
+	output_dir = args.output_path
+	dataset_eval_dir = args.dataset_eval_path
+	infer_chan = args.infer_channels
+
+	if model_type == 'Pre-trained':
+		model_type = '--restore_directory /home/sinadabiri/venvs/in-silico-labeling-master/isl/checkpoints'
+	else:
+		model_type = ' '
+	
+	# ----Confirm given folders exist--
+
+	assert os.path.exists(dataset_eval_dir), 'Confirm the given path for images directory exists.'
+	assert os.path.exists(output_dir), 'Confirm the given path for output images directory exists.'
+
+	date_time = datetime.now().strftime("%m-%d-%Y_%H:%M")
+
+	print ('\n The Evaluation Directory is:')
+	print (dataset_eval_dir)
+	print ('\n The Output Directory is:')
+	print (output_dir)
+	print ('\n ')
+	# # Confirm given folders exist
+	# if not os.path.exists(input_path):
+	#     print 'Confirm the given path for data exists.'
+	# assert os.path.exists(input_path), 'Confirm the given path for data exists.'
+	# if not os.path.exists(output_path):
+	#     print 'Confirm the given path for results exists.'
+	# assert os.path.exists(output_path), 'Confirm the given path for results exists.'
+	# assert input_path != output_path, 'With new well subdirectory requirement, output destination must be different than input.'
+
+	main()
+
+
--- ISL_training.py
+++ ISL_training.py
@@ -8,39 +8,36 @@ from time import strftime
 def main():
 
 	# ~/venvs/tensorflow/SinaFlow/bin/activate
-	# ~/SinaTesting3/bin/activate
-	VirtualEnvPathActive = 'source ~/venvs/tensorflow/SinaFlow/bin/activate; '
+	virtual_env_path_active = 'source ~/venvs/tensorflow/SinaFlow/bin/activate;'
 	# /home/sinadabiri/venvs/in-silico-labeling-master
-	# /home/sinadabiri/SinaTesting3/in-silico-labeling
-	# The directory where the ISL program is
-	BaseDirectoryPath = 'cd /home/sinadabiri/venvs/in-silico-labeling-master; '
+	base_directory_path = 'cd /home/sinadabiri/venvs/in-silico-labeling-master; '
 	
-	cmd1 = [BaseDirectoryPath + 'bazel shutdown;']
+	cmd1 = [base_directory_path + 'bazel shutdown;']
 	process1 = subprocess.Popen(cmd1, shell=True, stdout=subprocess.PIPE)
 	process1.wait()
 
 	# The directory where the checkpoints and output prediction image will be saved at.
-	BaseDir = 'export BASE_DIRECTORY=/mnt/finkbeinernas/robodata/Sina/in-silico-labeling/isl; '
-
-	cmd2 = [BaseDirectoryPath + VirtualEnvPathActive + BaseDir + 'bazel run isl:launch -- \
+	base_dir = 'export BASE_DIRECTORY=/mnt/finkbeinernas/robodata/Sina/in-silico-labeling/isl; '
+	
+	cmd2 = [virtual_env_path_active + base_directory_path + base_dir + 'bazel run isl:launch -- \
 	  --alsologtostderr \
 	  --base_directory $BASE_DIRECTORY \
 	  --mode TRAIN \
 	  --metric LOSS \
 	  --master "" \
-	  --restore_directory '+ CheckpointDir + ' \
-	  --output_directory '+ OutputDir + ' \
+	  --restore_directory '+ checkpoint_dir + ' \
+	  --output_path '+ output_dir + ' \
 	  --read_pngs \
-	  --dataset_train_directory ' + DatasetTrainDir + ' \
-	  --UntilStep ' + TillStep + ' \
-    > ' + OutputDir + '/training_output_'+ DateTime +'_B2images.txt \
-    2> ' + OutputDir + '/training_error_'+ DateTime +'_B2images.txt;' ]
+	  --dataset_train_directory ' + dataset_train_dir + ' \
+	  --until_step ' + till_step + ' \
+    > ' + output_dir + '/training_output_'+ date_time +'_B2images.txt \
+    2> ' + output_dir + '/training_error_'+ date_time +'_B2images.txt;']
 
 	process2 = subprocess.Popen(cmd2, shell=True, stdout=subprocess.PIPE)
 	process2.wait()
-	output1 = process2.communicate()[0]
+	output2 = process1.communicate()[0]
 
-	cmd3 = [BaseDirectoryPath + 'bazel shutdown;']
+	cmd3 = [base_directory_path + 'bazel shutdown;']
 	process3 = subprocess.Popen(cmd3, shell=True, stdout=subprocess.PIPE)
 	process3.wait()
 
@@ -48,10 +45,11 @@ def main():
 
 	# cmd2 = ['tensorboard --logdir /home/sinadabiri/venvs/in-silico-labeling-master/isl']
 	# process2 = subprocess.Popen(cmd2, shell=True, stdout=subprocess.PIPE)
+	# process2.wait()
 	# output2 = process2.communicate()[0]
 	
 	print ("The Output Directory is:")
-	print (OutputDir)
+	print (output_dir)
 	# print ("The Link to TensorBoard is:")
 	# print (output2)
 
@@ -62,29 +60,26 @@ if __name__ == '__main__':
   
   # ----Parser-----------------------
 	parser = argparse.ArgumentParser(description="ISL Testing.")
-	parser.add_argument("DatasetTrainPath", help="Folder path to images directory.")
-	parser.add_argument("CheckpointPath", help="Folder path to checkpoint directory.")
-	parser.add_argument("OutputPath", help="Output Image Folder location.")
-	parser.add_argument("TillStep", help="Train till step number mentioned.")
+	parser.add_argument("dataset_train_path", help="Folder path to images directory.")
+	parser.add_argument("checkpoint_path", help="Folder path to checkpoint directory.")
+	parser.add_argument("output_path", help="Output Image Folder location.")
+	parser.add_argument("till_step", help="Train till step number mentioned.")
 	parser.add_argument("outfile", help="Folder path to images directory.")
 	args = parser.parse_args()
 
   # ----Initialize parameters------------------
-	DatasetTrainDir = args.DatasetTrainPath
-	CheckpointDir = args.CheckpointPath
-	OutputDir = args.OutputPath
-	TillStep = args.TillStep
+	dataset_train_dir = args.dataset_train_path
+	checkpoint_dir = args.checkpoint_path
+	output_dir = args.output_path
+	till_step = args.till_step
 	outfile = args.outfile
 
-
 	# ----Confirm given folders exist--
-	assert os.path.exists(DatasetTrainDir), 'Confirm the given path for images directory exists.'
-	assert os.path.exists(CheckpointDir), 'Confirm the given path for images directory exists.'
-	assert os.path.exists(OutputDir), 'Confirm the given path for output images directory exists.'
+	assert os.path.exists(dataset_train_dir), 'Confirm the given path for images directory exists.'
+	assert os.path.exists(checkpoint_dir), 'Confirm the given path for images directory exists.'
+	assert os.path.exists(output_dir), 'Confirm the given path for output images directory exists.'
 	
-	DateTime = datetime.now().strftime("%m-%d-%Y_%H:%M")
-
-
-
+	date_time = datetime.now().strftime("%m-%d-%Y_%H:%M")
+	
 	main()
 
