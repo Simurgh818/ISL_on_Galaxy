9d0daacdb5af842b5aa12696e990176b4428cf18
Simurgh818 <sina.dabiri@berkeley.edu>
2018-10-09 14:46:43 -0700

    updated configure file was added.

--- configure.py
+++ configure.py
@@ -1,19 +1,19 @@
 ''' This interface sends Unix commands to run In-Silico Labeling TensorFlow program (Christiansen et al 2018). There are two modes: Predicting and Training. This script is to set the paths for virtual environment to run the programs, the path to checkpoints, image dataset, and output folder.
 '''
-# This is for Sina's computer 
-
-
+'''This configuration is for Sina's computer '''
+# /finkbeiner/imaging/smb-robodata/Sina
 
 virtual_environment = '/home/sinadabiri/venvs/tensorflow/SinaFlow/bin/activate'
-base_directory = '/home/sinadabiri/venvs/in-silico-labeling-master/'
-check_points = '/mnt/finkbeinernas/robodata/Sina/checkpoints'
-output_path = '/mnt/finkbeinernas/robodata/Sina/LogFiles'
-dataset_prediction = '/mnt/finkbeinernas/robodata/Sina/data_sample/condition_b_sample'
+base_directory = '/home/sinadabiri/venvs/in-silico-labeling-master'
+model_location = '/mnt/finkbeinernas/robodata/Sina/checkpoints'
+# output_path = '/mnt/finkbeinernas/robodata/Sina/LogFiles'
+# dataset_prediction = '/mnt/finkbeinernas/robodata/Sina/data_sample/condition_b_sample'
 dataset_training = '/mnt/finkbeinernas/robodata/Sina/data_sample/condition_e_sample_B2'
 
+'''This part is for Server '''
 # virtual_environment = '/finkbeiner/imaging/home/fbgalaxy/.virtualenvs/insilico/bin/activate'
-# check_points = '/finkbeiner/imaging/smb-robodata/Sina/checkpoints'
+# base_directory = '/finkbeiner/imaging/home/in-silico/in-silico-labeling/'
+# model_location = '/finkbeiner/imaging/home/in-silico/checkpoints'
 # output_path = '/finkbeiner/imaging/smb-robodata/Sina/LogFiles'
-# dataset_prediction = '/finkbeiner/imaging/smb-robodata/Sina/data_sample/condition_b_sample'
-# dataset_training = '/finkbeiner/imaging/smb-robodata/Sina/data_sample/condition_e_sample_B2'
-
+# dataset_prediction = '/finkbeiner/imaging/home/in-silico/datasets/condition_b_sample'
+# dataset_training = '/finkbeiner/imaging/home/in-silico/datasets/condition_e_sample_B2'
